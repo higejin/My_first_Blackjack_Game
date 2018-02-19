1fef6401079886c4042c8c86662b240712a0a156
nimjim <thtetp@gmail.com>
2018-02-19 21:50:57 +0900

    大幅に修正

--- Blackjack.py
+++ Blackjack.py
@@ -1,240 +1,224 @@
 
 import random
 
-card = ['sA', 's2','s3','s4','s5','s6','s7','s8','s9','s10','sJ','sQ','sK',
-        'hA', 'h2','h3','h4','h5','h6','h7','h8','h9','h10','hJ','hQ','hK',
-        'cA', 'c2','c3','c4','c5','c6','c7','c8','c9','c10','cJ','cQ','cK',
-        'dA', 'd2','d3','d4','d5','d6','d7','d8','d9','d10','dJ','dQ','dK']
-num = {'sA':1,'s2':2,'s3':3,'s4':4,'s5':5,'s6':6,'s7':7,'s8':8,'s9':9,'s10':10,'sJ':10,'sQ':10,'sK':10,
-       'hA':1,'h2':2,'h3':3,'h4':4,'h5':5,'h6':6,'h7':7,'h8':8,'h9':9,'h10':10,'hJ':10,'hQ':10,'hK':10,
-       'cA':1,'c2':2,'c3':3,'c4':4,'c5':5,'c6':6,'c7':7,'c8':8,'c9':9,'c10':10,'cJ':10,'cQ':10,'cK':10,
-       'dA':1,'d2':2,'d3':3,'d4':4,'d5':5,'d6':6,'d7':7,'d8':8,'d9':9,'d10':10,'dJ':10,'dQ':10,'dK':10,}
+suits = ['h', 's', 'c', 'd']
+ranking = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+card_val = {'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10}
 
-class Player(object):
+class Card:
 
-    def __init__(self,bankroll=100):
-        self.bankroll = bankroll
-        self.hand = []
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
 
-    def check_bankroll(self):
-        print("Your bankroll is : " + str(player1.bankroll))
+    def grab_rank(self):
+        return self.rank
 
-    def add_bankroll(self,amount):
-        self.bankroll += amount
+    def draw(self):
+        print(self.suit + self.rank, end="")
 
-    def bet(self):
-        while True:
-            self.betAmount = int(input("How much do you bet? : "))
-            if self.betAmount > self.bankroll:
-                print("Over betting.")
-                continue
-            else:
-                print("You are now betting " + str(self.betAmount))
-                break
 
-    def show_hand(self):
-        print("Player's hand : ", end = "")
-        for mycard in self.hand:
-            print(mycard + "  ", end = "")
+class Hand:
 
-        print(" (" + self.check_hand() + ")")
-
-    def check_hand(self):
-        x = 0
-        includeA = False
-
-        for i in self.hand:
-            if i == ('sA' or 'hA' or 'cA' or 'dA'):
-                includeA = True
-            x += num[i]
-
-        if includeA == True and x < 12:
-            x += 10
-
-        if x > 21:
-            return 'Bust!'
-
-        elif len(self.hand) == 2 and x == 21:
-            return 'Natural 21!'
-
-        else:
-            return str(x)
-
-class Dealer(object):
     def __init__(self):
         self.hand = []
+        self.value = 0
+        self.includeAce = False
 
-    def show_firsthand(self):
-        print("Dealer's hand : " + self.hand[0] + " + Hole card.")
+    def card_add(self, card):
+        self.hand.append(card)
 
-    def show_hand(self):
-        print("Dealer's hand : ", end ="")
-        for mycard in self.hand:
-            print(mycard + "  ", end ="")
+        # Check for Aces (Ace can also be 11)
+        if card.rank == 'A':
+            self.includeAce = True
 
-        print(" (" + str(self.check_hand()) + ")")
+        self.value += card_val[card.rank]
 
-    def check_hand(self):
-        x = 0
-        includeA = False
-
-        for i in self.hand:
-            if i == ('sA' or 'hA' or 'cA' or 'dA'):
-                includeA = True
-            x += num[i]
-
-        if includeA and x < 12:
-            x += 10
-
-        if x > 21:
-            return 'Bust!'
-
-        elif len(self.hand) == 2 and x == 21:
-            return 'Natural 21!'
-
+    def calc_val(self):
+        # Calculate the value of the hand, make aces an 11 if they don't bust the hand
+        if self.includeAce == True and self.value < 12:
+            return self.value + 10
         else:
-            return x
+            return self.value
 
-class Decks(object):
+    def show_hand(self, man = 'You', hidden = False):
+        print(man + " : ", end="")
+        # Hole card is dealer's card which is hidden first
+        if hidden == True:
+            self.hand[0].draw()
+            print(" + Hole card")
+        else:
+            for x in range(0, len(self.hand)):
+                self.hand[x].draw()
+                print(" ", end="")
+            print("  (" + str(self.calc_val()) + ") ")
 
-    def __init__(self, numberOfDecks = 1):
-        self.numberOfDecks = numberOfDecks
-        self.card = ['sA', 's2','s3','s4','s5','s6','s7','s8','s9','s10','sJ','sQ','sK',
-                     'hA', 'h2','h3','h4','h5','h6','h7','h8','h9','h10','hJ','hQ','hK',
-                     'cA', 'c2','c3','c4','c5','c6','c7','c8','c9','c10','cJ','cQ','cK',
-                     'dA', 'd2','d3','d4','d5','d6','d7','d8','d9','d10','dJ','dQ','dK']
+class Decks:
 
-    def random_pic(self):
-        while True:
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranking:
+                self.deck.append(Card(suit,rank))
 
-            try:
-                selectedCard = random.choice(self.card)
+    def shuffle(self):
+        random.shuffle(self.deck)
 
-            except:
-                print("there is no card.")
-                exit()
+    def deal(self):
+        return self.deck.pop()
+
+
+class Player:
+
+    def __init__(self, bankroll = 100):
+        self.bankroll = bankroll
+
+    def check_bankroll(self):
+        print("Your bankroll is now : " + str(player1.bankroll))
+
+    def add_bankroll(self, amount):
+        self.bankroll += amount
+
+    def make_bet(self):
+        self.bet = 0
+        while self.bet == 0:
+            bet_amount = int(input("How much do you bet? : "))
+            if bet_amount > self.bankroll:
+                print("Invalid bet, you only have " + str(self.bankroll) + " remaining")
+            elif bet_amount < minimum_bet:
+                print("Invalid bet, minimum bet is " + str(minimum_bet))
             else:
-                self.card.remove(selectedCard)
-                return selectedCard
+                self.bet = bet_amount
 
 
-
-new_deck = Decks()
+minimum_bet = 10
 player1 = Player(bankroll=1000)
-dealer = Dealer()
 
-def deal_cards(player):
-    player.hand.clear()
-    player.hand.append(new_deck.random_pic())
-    player.hand.append(new_deck.random_pic())
 
-def hit_cards(player):
-    player.hand.append(new_deck.random_pic())
-
-def determine_outcome(p,d):
-    if p.check_hand() == 'Bust!':
-        print('You lose!')
-        p.add_bankroll(-(p.betAmount))
-
-    elif p.check_hand() == 'Natural 21!':
-        if d.check_hand() == 'Natural 21!':
-            print('push')
+def determine_outcome(p_hand,d_hand,p):
+    print("")
+    if p_hand.calc_val() > 21:
+        print('Busted! You lose!')
+        p.add_bankroll(-(p.bet))
+    elif p_hand.calc_val() == 21 and len(p_hand.hand) == 2:
+        if d_hand.calc_val() == 21 and len(d_hand.hand) == 2:
+            print('Both Natural Blackjack!! push!!')
         else:
-            print('You win!')
-            p.add_bankroll(p.betAmount)
+            print('Natural Blackjack! You win!')
+            p.add_bankroll(p.bet)
     else:
-        if d.check_hand() == 'Natural 21!':
+        if d_hand.calc_val() == 21 and len(d_hand.hand) == 2:
+            print('Dealer got Natural Blackjack! You lose!')
+            p.add_bankroll(-(p.bet))
+        elif d_hand.calc_val() > 21:
+            print('Dealer busts! You win!')
+            p.add_bankroll(p.bet)
+        elif p_hand.calc_val() == d_hand.calc_val():
+            print('Tied up, push!')
+        elif p_hand.calc_val() < d_hand.calc_val():
             print('You lose!')
-            p.add_bankroll(-(p.betAmount))
-        elif d.check_hand() == 'Bust!':
-            print('You win!')
-            p.add_bankroll(p.betAmount)
-        elif d.check_hand() == int(p.check_hand()):
-            print('push')
-        elif int(d.check_hand()) > int(p.check_hand()):
-            print('You lose!')
-            p.add_bankroll(-(p.betAmount))
+            p.add_bankroll(-(p.bet))
         else:
             print('You win!')
-            p.add_bankroll(p.betAmount)
-    print(" ")
+            p.add_bankroll(p.bet)
+    print("")
 
 def replay():
-
-    return input('Do you want to continue? Enter Yes or No: ').lower().startswith('y')
+    x = input('Do you want to continue? Enter Yes or No: ').lower().startswith('y')
+    print("")
+    return x
 
 
 while True:
 
+    deck = Decks()
+    deck.shuffle()
+    deck.shuffle()
+    deck.shuffle()
+
     player1.check_bankroll()
-    player1.bet()
+    player1.make_bet()
 
-    deal_cards(dealer)
-    dealer.show_firsthand()
+    # Set up or Clear both player and dealer hands
+    player1_hand = Hand()
+    dealer_hand = Hand()
 
-    deal_cards(player1)
-    player1.show_hand()
+    # First deal
+    player1_hand.card_add(deck.deal())
+    player1_hand.card_add(deck.deal())
+    dealer_hand.card_add(deck.deal())
+    dealer_hand.card_add(deck.deal())
 
-    sel = input("Please select: 'hit' or 'stand' or 'double down' or 'surrender'.").lower()
-    print(" ")
-    if sel.startswith('su'):
-        player1.add_bankroll(-(player1.betAmount)//2)
+    print("")
+    player1_hand.show_hand('You   ')
+    dealer_hand.show_hand('Dealer', hidden=True)
+    print("")
+
+    while True:
+        select = input("Please Enter ('h' to hit / 'st' to stand / 'd' to double down / 'su' to surrender) : ").lower()
+        if select.startswith('d') and player1.bet * 2 > player1.bankroll:
+            print("You are betting now " + str(player1.bet))
+            print("and you only have " + str(player1.bankroll) + " now, so you can't double down.")
+        else:
+            break
+    print("")
+
+    # Surrender: fold hand and receive half of bet back
+    if select.startswith('su'):
+        player1.add_bankroll(-(player1.bet)//2)
+        print("You surrendered.")
+        print("")
         if replay():
             continue
         else:
+            print("Your final bankroll is " + str(player1.bankroll))
+            print("Good bye!")
             break
-    elif sel.startswith('h'):
-        hit_cards(player1)
-        dealer.show_firsthand()
-        player1.show_hand()
-        while player1.check_hand() != 'Bust!':
-            print(" ")
+
+    # Hit: ask another card (you can do repeatedly until you bust)
+    elif select.startswith('h'):
+        player1_hand.card_add(deck.deal())
+        player1_hand.show_hand('You   ')
+        dealer_hand.show_hand('Dealer', hidden=True)
+        while player1_hand.calc_val() <= 21:
+            print("")
             if input("Hit again? Enter Y/N : ").lower().startswith('y'):
-                hit_cards(player1)
-                dealer.show_firsthand()
-                player1.show_hand()
+                player1_hand.card_add(deck.deal())
+                print("")
+                player1_hand.show_hand('You   ')
+                dealer_hand.show_hand('Dealer', hidden=True)
             else:
                 break
-    elif sel.startswith('d'):
-        player1.betAmount *= 2
-        print("You are now betting " + str(player1.betAmount))
-        hit_cards(player1)
-        dealer.show_firsthand()
-        player1.show_hand()
-        while player1.check_hand() != 'Bust!':
-            print(" ")
-            if input("Hit again? Enter Y/N : ").lower().startswith('y'):
-                hit_cards(player1)
-                dealer.show_firsthand()
-                player1.show_hand()
-            else:
-                break
+
+    # Double down: double your bet and must hit once and you can hit only one time
+    elif select.startswith('d'):
+        player1.bet *= 2
+        print("You are now betting " + str(player1.bet))
+        player1_hand.card_add(deck.deal())
+        player1_hand.show_hand('You   ')
+        dealer_hand.show_hand('Dealer', hidden=True)
+
+    # Stand: hold hand and end turn
     else:
         pass
 
-    print(" ")
-    print("open dealer's hole card.")
-    print(" ")
 
-    dealer.show_hand()
-    player1.show_hand()
+    print("")
+    player1_hand.show_hand('You   ')
+    dealer_hand.show_hand('Dealer', hidden=False)
 
-    while True:
-        print(" ")
-        if dealer.check_hand() == 'Bust!':
-            break
-        elif dealer.check_hand() == 'Natural 21!':
-            break
-        elif dealer.check_hand() > 16:
-            break
-        else:
-            hit_cards(dealer)
-            dealer.show_hand()
-            player1.show_hand()
+    # Soft 17 rule: the dealer must hit until the cards total 17 or more points
+    while dealer_hand.calc_val() < 17:
+        dealer_hand.card_add(deck.deal())
+        print("")
+        player1_hand.show_hand('You   ')
+        dealer_hand.show_hand('Dealer', hidden=False)
 
-    print(" ")
-    determine_outcome(player1,dealer)
+    determine_outcome(player1_hand, dealer_hand, player1)
+
     if replay():
         continue
     else:
+        print("Your final bankroll is " + str(player1.bankroll))
+        print("Good bye!")
         break
